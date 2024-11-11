f714c59f8a8f6c2a079946948eccb877ff8f194b
AcidBomb <47693252+Ac1dBomb@users.noreply.github.com>
2024-11-11 12:59:30 -0500

    cv_utils.py

--- cv_utils.py
+++ cv_utils.py
@@ -1,3 +1,14 @@
-Core code goes here.  Modular approach so we can make this more useful.
+# cv_utils.py
+import cv2
+import torch 
+import tensorflow as tf  # For TensorFlow Lite
 
-More to come Friday
+# ... other imports
+
+def detect_objects_yolov7(frame):
+  # ... YOLOv7 object detection implementation ...
+
+def detect_objects_tflite(frame):
+  # ... TensorFlow Lite object detection implementation ...
+
+# ... other computer vision functions (tracking, pose estimation, etc.)
